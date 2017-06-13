1b6ac6fae4107c16c0f652b4d2c9801b564ed703
孔昀晖 <1032316751@qq.com>
2017-06-13 11:02:22 +0800

    熔断监控 Hystrix Dashboard

--- src/main/java/com/kyh/SpringCloudConsumerApplication.java
+++ src/main/java/com/kyh/SpringCloudConsumerApplication.java
@@ -2,12 +2,16 @@ package com.kyh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient // 启用服务注册与发现
 @EnableFeignClients // 启用feign进行远程调用
+@EnableHystrixDashboard // 启用熔断监控面板
+@EnableCircuitBreaker // 启用断路器
 public class SpringCloudConsumerApplication {
 
 	public static void main(String[] args) {
